1e5ebe24dcaa7a32688a32953dd1a9ff394efbb4
Natalia Shashina <62610792+natasergeevna@users.noreply.github.com>
2021-05-07 17:13:15 +0300

    Update main.cpp

--- poisk_loop/main.cpp
+++ poisk_loop/main.cpp
@@ -1,155 +1,89 @@
 #include <iostream>
+#include <cstdio>
+#include <queue>
+
 using namespace std;
 
-struct Node
-{
-    int val;
-    Node *next;
-    Node(int _val = 0, Node *_next = nullptr):val(_val), next(_next) {}
-};
+const int max_size = 100;
+int n,m;
+char mas[max_size][max_size];
 
-class List
+void input()
 {
-    Node *first;
-public:
-    List():first(nullptr) {}
-    
-    void add(int a)
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
     {
-        Node *p = new Node(a, first);
-        first = p;
-    }
-    
-    void print()
-    {
-        Node *t = first;
-        while (t != nullptr)
+        getchar();   // переход на новую строку
+        for (int j = 0; j < m; j++)
         {
-            cout << t->val << " ";
-            t = t->next;
-        }
-        cout << endl;
-    }
-    
-    void addcycle(int size)
-    {
-        Node *foll = first;
-        Node *prev = nullptr;
-        while (foll != nullptr)
-        {
-            prev = foll;
-            foll = foll->next;
-        }
-        Node *f = prev;
-        for(int i = 0; i < size; i++)
-        {
-            prev->next = new Node(i, nullptr);
-            prev = prev->next;
-        }
-        prev->next = f;
-    }
-    
-    bool hasLoop1()  //2й двигаем 2 раза 1й один
-    {
-        if(first == nullptr)
-            return false;
-        Node *slow = first;
-        Node *fast = first->next;
-        while(true)
-        {
-            slow = slow->next;
-            if(fast->next != nullptr)
-                fast = fast->next->next;
-            else
-                return false;
-            if((slow == nullptr) || (fast == nullptr))
-                return false;
-            if(slow == fast)
-                return true;
+            mas[i][j] = getchar();
         }
     }
-    
-    bool hasLoop2() //1й двигается на 1,2,3.. элемента
+}
+
+struct point   //координата точки
+{
+    int x, y;
+    point() {}
+    point(int X, int Y)
     {
-        if(first == nullptr)
-            return false;
-        int i = 1;
-        Node *p = first;
-        while(true)
-        {
-            Node *p1 = p;
-            for(int j = 0; j < i; j++)
-            {
-                p1 = p1->next;
-                if(p1 == nullptr)
-                    return false;
-            }
-            if(p == p1)
-                return true;
-            p = p1;
-            i++;
-        }
-    }
-    
-    bool hasLoop3()  //изменение направления указателей
-    {
-        Node *p = nullptr;
-        Node *pcurr = nullptr;
-        Node *pnext = nullptr;
-        bool isloop = false;
-        if (first == nullptr)
-            return false;
-        if (first->next == nullptr)
-            return false;
-        pcurr = first;
-        pnext = first->next;
-        while(true)
-        {
-            pcurr->next = p;
-            if (pnext == nullptr)
-            {
-                if (pcurr == first)
-                {
-                    isloop = true;
-                }
-                Node *tmp = pnext;
-                pnext = p;
-                p = tmp;
-                break;
-            }
-            p = pcurr;
-            pcurr = pnext;
-            pnext = pnext->next;
-        }
-        while(true)
-        {
-            pcurr->next = p;
-            if (pnext == nullptr)
-                break;
-            p = pcurr;
-            pcurr = pnext;
-            pnext = pnext->next;
-        }
-        return isloop;
+        x = X; y = Y;
     }
 };
 
-
+int moveX[4] = {0, 1, 0, -1};
+int moveY[4] = {1, 0, -1, 0};   //для номера куска
+bool correct(int x, int y)
+{
+    if (x < 0 || y < 0)
+        return false;
+    if (x >= n || y >= m)
+        return false;
+    return true;
+}
+void bfs(int x, int y)    //обход в ширину
+{
+    queue<point> q;
+    q.push(point(x,y));
+    while (!q.empty())
+    {
+        point cur = q.front();   //текущий элемент 1-й
+        q.pop();  //удаляем 1-й элемент
+        for (int i = 0; i < 4; i++)
+        {
+            int x = cur.x + moveX[i];  //координата по х
+            int y = cur.y + moveY[i];  //координата по у
+            if (correct(x,y) && mas[x][y] == '#')
+            {
+                q.push(point(x,y));
+                mas[x][y] = '.';
+            }
+        }
+    }
+}
+void solve()  //считаем кол-во островков
+{
+    int amount = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (mas[i][j] == '#')
+            {
+                mas[i][j] = '.';
+                bfs(i,j);
+                amount++;
+            }
+        }
+    }
+    cout << amount;
+}
 int main()
 {
-    List l;
-    for(int i = 0; i < 5; i++)
-        l.add(i+1);
-    l.print();
-    cout << "First Loop check w/o cycle: " << l.hasLoop1() << endl;
-    cout << "Second Loop check w/o cycle: " << l.hasLoop2() << endl;
-    cout << "Third Loop check w/o cycle: " << l.hasLoop3() << endl;
-    l.addcycle(5);
-    cout << "First Loop check w/ cycle: " << l.hasLoop1() << endl;
-    cout << "Second Loop check w/ cycle: " << l.hasLoop2() << endl;
-    cout << "Third Loop check w/ cycle: " << l.hasLoop3() << endl;
-    for(int i = 0; i < 5; i++)
-        l.add(i+1);
-    l.print();
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    input();
+    solve();
     return 0;
 }
